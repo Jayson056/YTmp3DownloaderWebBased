b82247dd89c9e7e892560309e1ea7b848cd39853
Jayson Apable Combate <103288096+Jayson056@users.noreply.github.com>
2024-12-18 09:43:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -113,4 +113,4 @@ def download_mp4():
     return download_file(link, format='mp4')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=0000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
